192ac968cdcbf038175ee3c4cb2666f0233b3b33
rlouvet <robinlouvet@gmail.com>
2018-01-23 13:34:57 -0500

    UTF-8 encoding is more universal. Also it is the default encoding for the Spark Streaming Python API.

--- data_generator/generator.py
+++ data_generator/generator.py
@@ -29,7 +29,7 @@ class Generator:
 		kafka_server = os.environ['KAFKA_SERVER']
 		kafka_port = os.environ['KAFKA_PORT']
 		self.producer = KafkaProducer(bootstrap_servers=':'.join([kafka_server, kafka_port]),
-		 value_serializer=lambda m: m.encode('ascii'))
+		 value_serializer=lambda m: m.encode('utf8'))
 
 		sitemap_cfg = config['sitemap']
 		walker_cfg = config['walker']
