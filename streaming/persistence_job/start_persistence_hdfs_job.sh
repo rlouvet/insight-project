0d36f07addbd2234ebf671d4740c471d48937afe
rlouvet <robinlouvet@gmail.com>
2018-01-23 15:27:59 -0500

    Fixing typo

--- streaming/persistence_job/start_persistence_hdfs_job.sh
+++ streaming/persistence_job/start_persistence_hdfs_job.sh
@@ -1,1 +1,1 @@
-$SPARK_HOME/bin/spark-submit --master spark://$SPARK_MASTER:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.1 persistence _hdfs_job.py
+$SPARK_HOME/bin/spark-submit --master spark://$SPARK_MASTER:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.1 persistence_hdfs_job.py
