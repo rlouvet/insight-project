9bb433d1accf04a66aaa0400524ed50f66c6022a
rlouvet <robinlouvet@gmail.com>
2018-01-25 10:51:03 -0500

    Using simpler config file

--- streaming/persistence_job/persistence_hdfs_job.py
+++ streaming/persistence_job/persistence_hdfs_job.py
@@ -7,29 +7,22 @@ during my Insight Fellowship program (NYC Jan 2018).
 It can persist customer click-stream data into a master dataset on HDFS.
 """
 import os
-import yaml
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
-zkQuorum = os.environ['ZK_QUORUM']
-
-with open("config.yml", 'r') as ymlfile:
-	cfg = yaml.load(ymlfile)
-
 if __name__ == "__main__":
+
+	with open("config.txt", 'r') as f:
+		zkQuorum, topic = f.readline().split('')
 
     sc = SparkContext(appName="spark_streaming_hdfs")
     ssc = StreamingContext(sc, 1)
-    
-    topic = cfg['topic']
 
-    kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
-    lines = kvs.map(lambda x: x[1])
-    counts = lines.flatMap(lambda line: line.split(" ")) \
-        .map(lambda word: (word, 1)) \
-        .reduceByKey(lambda a, b: a+b)
-    counts.pprint()
+    kafka_stream = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
+    parsed = kafka_stream.map(lambda v: json.loads(v[1]))
+    count = parsed.count()
+    count.pprint()
 
     ssc.start()
     ssc.awaitTermination()
