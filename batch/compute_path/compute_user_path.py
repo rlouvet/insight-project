e31b03a19835a694eb277439f2e20002b5855281
rlouvet <robinlouvet@gmail.com>
2018-01-25 16:17:34 -0500

    First attempt on batch job

---
+++ batch/compute_path/compute_user_path.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+[Customer Support Percolation] Persistence to HDFS Job
+This script is part of the "Customer Support Percolation" project developped
+during my Insight Fellowship program (NYC Jan 2018).
+It can process the master click-stream dataset to compute user paths.
+"""
+import os, json
+from pyspark import SparkContext, SparkConf
+
+bucket_name = os.environ['BUCKET_NAME']
+
+if __name__ == "__main__":
+
+    conf = SparkConf().setAppName('Batch - Compute User Path')
+    sc = SparkContext(conf=conf)
+    cs = sc.textFile('s3a://' + bucket_name + '/parsed')
+    print('Count: ' + cs.take(100))
---
+++ batch/compute_path/start_compute_user_path.sh
@@ -0,0 +1,1 @@
+$SPARK_HOME/bin/spark-submit --master spark://$SPARK_MASTER:7077 compute_user_path.py
