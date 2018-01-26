fe3df7fcab8bff4ddd40c251ff9c6a8348ad9bd8
rlouvet <robinlouvet@gmail.com>
2018-01-26 15:35:15 -0500

    Make it leaner

--- frontend/app/views/main.py
+++ frontend/app/views/main.py
@@ -7,21 +7,3 @@ import random
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
-
-
-@app.route('/map')
-def map():
-    return render_template('map.html', title='Map')
-
-
-@app.route('/map/refresh', methods=['POST'])
-def map_refresh():
-    points = [(random.uniform(48.8434100, 48.8634100),
-               random.uniform(2.3388000, 2.3588000))
-              for _ in range(random.randint(2, 9))]
-    return jsonify({'points': points})
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html', title='Contact')
