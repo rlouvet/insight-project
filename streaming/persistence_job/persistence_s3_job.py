88a32c3bc1ba9524dfcde0498d350b41e8a2f561
rlouvet <robinlouvet@gmail.com>
2018-01-31 15:53:39 -0500

    Better naming

--- streaming/persistence_job/persistence_s3_job.py
+++ streaming/persistence_job/persistence_s3_job.py
@@ -7,7 +7,7 @@ during my Insight Fellowship program (NYC Jan 2018).
 It can persist customer click-stream data into a master dataset on AWS S3.
 """
 import os, json
-import datetime, time
+import datetime
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
@@ -17,6 +17,7 @@ topic = os.environ['TOPIC']
 bucket_name = os.environ['BUCKET_NAME']
 
 if __name__ == "__main__":
+    start_time = datetime.datetime.now()
 
     sc = SparkContext(appName="spark_streaming")
     ssc = StreamingContext(sc, 1)
@@ -25,7 +26,7 @@ if __name__ == "__main__":
 
     messages = kafka_stream.map(lambda v: v[1])
     now = datetime.datetime.now()
-    messages.saveAsTextFiles('s3a://' + bucket_name + '/clickstreams-' + now.strftime("%Y%m%dT%H%M%S"))
+    messages.saveAsTextFiles('s3a://' + bucket_name + '/clickstreams-' + start_time.strftime("%Y%m%dT%H%M%S"))
 
     count = messages.count()
     count.pprint()
