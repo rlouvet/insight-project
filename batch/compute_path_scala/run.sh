5e02b70f320bc16af139abb81bc993b5492085c8
rlouvet <robinlouvet@gmail.com>
2018-02-01 17:52:47 -0500

    Accessing s3 buckets with the new scala batch app

--- batch/compute_path_scala/run.sh
+++ batch/compute_path_scala/run.sh
@@ -1,1 +1,1 @@
-spark-submit --master local --class Main --jars ./lib/CountMinSketchOnSpark-assembly-1.0.jar ./target/scala-2.11/testproject_2.11-1.0.jar
+spark-submit --master local --class Main --jars ./lib/CountMinSketchOnSpark-assembly-1.0.jar ./target/scala-2.11/testproject_2.11-1.0.jar 20180130T125955-1517319
--- batch/compute_path_scala/src/main/scala/Main.scala
+++ batch/compute_path_scala/src/main/scala/Main.scala
@@ -6,17 +6,21 @@ import org.viirya.CountMinSketch._
 object Main{
     def main(args: Array[String]) {
 
+    val read_bucket_name = sys.env("READ_BUCKET_NAME")
+    val read_bucket_name = sys.env("WRITE_BUCKET_NAME")
+    val target_time = args(0)
+
     val spark = SparkSession
        .builder()
        .master("local[*]")
        .appName("Compute path app")
        .getOrCreate()
 
-    //import spark.sqlContext.implicits._
     // For implicit conversions like converting RDDs to DataFrames
     import spark.implicits._
 
-    val df = spark.read.json("/home/robin/Documents/insight/dev/insight-project/local/sample-data.json")
+    val df = spark.read.json("s3a://" + read_bucket_name
+        + "/clickstreams-" + target_time + "*")
 
     // Call API to calculate estimated frequencies of column "numbers"
     val results = countMinSketch(df, "userid").collect()
