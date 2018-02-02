cae4118fd7fcc3f374ad21a1a75283acb5e819e4
rlouvet <robinlouvet@gmail.com>
2018-02-01 19:39:06 -0500

    Handling two runtime environment for rapid development

--- batch/compute_path_scala/src/main/scala/Main.scala
+++ batch/compute_path_scala/src/main/scala/Main.scala
@@ -4,28 +4,49 @@ import org.apache.spark.sql.SparkSession
 import org.viirya.CountMinSketch._
 
 object Main{
+
+    val usage = """
+        Usage: --env [local|aws] --target [ex:20180130T125955-1517319]
+    """
+
     def main(args: Array[String]) {
+        if (args.length == 0) println(usage)
 
-    val read_bucket_name = sys.env("READ_BUCKET_NAME")
-    val write_bucket_name = sys.env("WRITE_BUCKET_NAME")
-    val target_time = args(0)
+        var current_environment = ""
+        var target = ""
+        args.sliding(2, 2).toList.collect {
+          case Array("--env", argEnv: String) => current_environment = argEnv
+          case Array("--target", argTarget: String) => target = argTarget
+        }
 
-    val spark = SparkSession
-       .builder()
-       .master("local[*]")
-       .appName("Compute path app")
-       .getOrCreate()
+        if (current_environment == "aws"){
+            val spark_master = sys.env("SPARK_MASTER")
+            val read_bucket_name = sys.env("READ_BUCKET_NAME")
+            val write_bucket_name = sys.env("WRITE_BUCKET_NAME")
+            read_target = "s3a://" + read_bucket_name + "/clickstreams-" + target_time + "*"
+        } else if (current_environment == "local") {
+            val spark_master = "local[*]"
+            var read_target = ""/home/robin/Documents/insight/dev/insight-project/local/sample-data.json"
+        } else {
+            println(usage)
+        }
 
-    // For implicit conversions like converting RDDs to DataFrames
-    import spark.implicits._
 
-    val df = spark.read.json("s3a://" + read_bucket_name
-        + "/clickstreams-" + target_time + "*")
+        val spark = SparkSession
+           .builder()
+           .master(spark_master)
+           .appName("Compute path app")
+           .getOrCreate()
 
-    // Call API to calculate estimated frequencies of column "numbers"
-    val results = countMinSketch(df, "userid").collect()
+        // For implicit conversions like converting RDDs to DataFrames
+        import spark.implicits._
 
-    results.foreach(println)
+        val df = spark.read.json(read_target)
+
+        // Call API to calculate estimated frequencies of column "numbers"
+        val results = countMinSketch(df, "userid").collect()
+
+        results.foreach(println)
 
     }
 }
