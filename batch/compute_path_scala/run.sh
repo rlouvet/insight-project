50dc3b4576d8e2f152cc71c6e809de4d2fc9cfeb
rlouvet <robinlouvet@gmail.com>
2018-02-01 19:52:18 -0500

    run script with AWS values

--- batch/compute_path_scala/run.sh
+++ batch/compute_path_scala/run.sh
@@ -1,1 +1,1 @@
-spark-submit --master local --class Main --jars ./lib/CountMinSketchOnSpark-assembly-1.0.jar ./target/scala-2.11/testproject_2.11-1.0.jar --env local --target /home/robin/Documents/insight/dev/insight-project/local/sample-data.json
+spark-submit --master local --class Main --jars ./lib/CountMinSketchOnSpark-assembly-1.0.jar ./target/scala-2.11/testproject_2.11-1.0.jar --env aws --target 20180130T125955-1517319
