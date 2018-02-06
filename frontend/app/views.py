8c7d6111503773eaf5069a235315745521a32d4d
rlouvet <robinlouvet@gmail.com>
2018-02-05 20:45:44 -0500

    Version working with bootstrap 4.0.0

--- frontend/app/views.py
+++ frontend/app/views.py
@@ -1,6 +1,33 @@
+from flask import render_template, jsonify
 from app import app
+import random
+import os, json
+import boto3
+import botocore
+
+env_mode = os.getenv('ENV_MODE', 'aws')
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return render_template("base.html")
+
+    if env_mode == 'aws':
+
+        bucket_name = os.environ['BUCKET_NAME']
+        file_name = os.environ['FILE_NAME']
+
+        s3 = boto3.resource('s3')
+        try:
+            s3.Bucket(bucket_name).download_file(file_name, '/tmp/data.json')
+            data = json.load(open('/tmp/data.json'))
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                raise
+    else:
+        file_path = os.environ['FILE_PATH']
+        data = json.load(open(file_path))
+
+
+    return render_template('base.html', title='Home', results=data)
--- frontend/tornadoapp.py
+++ frontend/tornadoapp.py
@@ -8,13 +8,13 @@ class MainHandler(RequestHandler):
     def get(self):
         self.write("This message comes from Tornado ^_^")
 
-        tr = WSGIContainer(app)
+tr = WSGIContainer(app)
 
-        application = Application([
-        (r"/tornado", MainHandler),
-        (r".*", FallbackHandler, dict(fallback=tr)),
-        ])
+application = Application([
+(r"/tornado", MainHandler),
+(r".*", FallbackHandler, dict(fallback=tr)),
+])
 
-        if __name__ == "__main__":
-            application.listen(80)
-            IOLoop.instance().start()
+if __name__ == "__main__":
+    application.listen(80)
+    IOLoop.instance().start()
