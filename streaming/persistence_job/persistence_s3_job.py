9dd1c9b8d251b63ffd1be03ce52620fea220993e
rlouvet <robinlouvet@gmail.com>
2018-02-07 02:17:20 -0500

    New version of the streaming job using spark-sql-kafka that appends successfully to console

--- streaming/persistence_job/persistence_s3_job.py
+++ streaming/persistence_job/persistence_s3_job.py
@@ -6,27 +6,64 @@ This script is part of the "Customer Support Percolation" project developped
 during my Insight Fellowship program (NYC Jan 2018).
 It can persist customer click-stream data into a master dataset on AWS S3.
 """
-import os, json
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
 
-brokers_dns_str = os.environ['KAFKA_SERVERS']
+kafka_servers = os.environ['KAFKA_SERVERS']
 topic = os.environ['TOPIC']
 bucket_name = os.environ['BUCKET_NAME']
 
 if __name__ == "__main__":
 
-    sc = SparkContext(appName="spark_streaming")
-    ssc = StreamingContext(sc, 1)
+    spark = (
+        SparkSession
+        .builder
+        .appName("Job that persists clickstream data from Kafka to CSV files on S3") \
+        .getOrCreate()
+    )
 
-    kafka_stream = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers_dns_str})
+    kafka_stream = (
+        spark
+        .readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", kafka_servers)
+        .option("subscribe", topic)
+        .load()
+    )
 
-    messages = kafka_stream.map(lambda v: v[1])
-    messages.saveAsTextFiles('s3a://' + bucket_name + '/clickstreams')
+    messages = kafka_stream.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
 
-    count = messages.count()
-    count.pprint()
+    schema = StructType(
+        [
+        StructField("epochtime", StringType()),
+        StructField("userid", StringType()),
+        StructField("pageid_origin", StringType()),
+        StructField("pageid_target", StringType()),
+        StructField("case_status", StringType())
+        ]
+    )
 
-    ssc.start()
-    ssc.awaitTermination()
+    parsed_messages = (
+        messages
+        .select(from_json(messages.value, schema).alias("json"))
+        .select(
+            col("json").getItem("epochtime").alias("epochtime"),
+            col("json").getItem("userid").alias("userid"),
+            col("json").getItem("pageid_origin").alias("pageid_origin"),
+            col("json").getItem("pageid_target").alias("pageid_target"),
+            col("json").getItem("case_status").alias("case_status")
+            )
+    )
+
+    query = (
+        parsed_messages
+        .writeStream
+        .outputMode("append")
+        .format("console")
+        #.option("path", "s3a://" + bucket_name + "/clickstreams")
+        .start()
+    )
+
+    query.awaitTermination()
