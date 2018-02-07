397468b00c264870478b4257803d4524119bd81a
rlouvet <robinlouvet@gmail.com>
2018-02-07 07:58:18 -0500

    Better master handling and file example

--- batch/compute_path/start_compute_user_path.sh
+++ batch/compute_path/start_compute_user_path.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$SPARK_HOME/bin/spark-submit --master spark://$SPARK_MASTER:7077 compute_user_path.py
+$SPARK_HOME/bin/spark-submit --master $SPARK_MASTER compute_user_path.py 20180130T125955-1517319
