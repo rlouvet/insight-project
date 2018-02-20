b19a044f4fe56a8df3ef92759edf8d305d26c584
rlouvet <robinlouvet@gmail.com>
2018-02-20 17:11:37 -0500

    Adding custom PathsPartitioner

--- batch/compute_path_scala/src/main/scala/Main.scala
+++ batch/compute_path_scala/src/main/scala/Main.scala
@@ -1,13 +1,13 @@
 //imports
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{ListBuffer, HashMap}
+import scala.math.{round, min, max, abs}
+
+import org.apache.spark.Partitioner
+import org.apache.spark.sql.Row
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.Row
 
-//import org.viirya.CountMinSketch._
-import com.clearspring.analytics.stream.frequency.CountMinSketch
-import java.util.Random
 
 object Main{
 
@@ -77,12 +77,68 @@ object Main{
             }
         }
 
-        val resolved_paths_df = user_agg_df.map(PathResolver.resolve).flatMap(e => e._2).toDF()
-        val paths_df = resolved_paths_df.selectExpr("CAST(value AS STRING)")
-        paths_df.createOrReplaceTempView("paths_rank")
-        val paths_rank_df = spark.sql("SELECT value,COUNT(*) as count FROM paths_rank GROUP BY value ORDER BY count DESC")
+        val resolved_paths_df = user_agg_df.map(PathResolver.resolve).flatMap(e => e._2)
+        val partitioned_paths_df = resolved_paths_df.rdd
+        .map(path => (path, 1))
+        .partitionBy(new PathPartitioner(30))
+        .reduceByKey(_+_)
+        .toDF("value","count")
+        .sort(desc("count"))
+        .limit(1000).write.json(write_target)
+
+        //val paths_df = resolved_paths_df.selectExpr("CAST(value AS STRING)")
+        //paths_df.createOrReplaceTempView("paths_rank")
+        //val paths_rank_df = spark.sql("SELECT value,COUNT(*) as count FROM paths_rank GROUP BY value ORDER BY count DESC")
 
         paths_rank_df.limit(1000).write.json(write_target)
 
     }
+}
+
+class PathPartitioner(num_parts: Int) extends Partitioner {
+    require(num_parts >= 1, s"Number of partitions ($num_parts) should be greater than 1.")
+    //This is a map that contains information about the pathlength data distribution
+    private val distribution_map: HashMap[Int,Double] = HashMap(
+        (1, 0.30),
+        (2, 0.21),
+        (3, 0.15),
+        (4, 0.10),
+        (5, 0.07)
+    )
+
+    // Enforce a minimum of 10 partitions
+    override def numPartitions: Int = max(10, num_parts)
+
+    //This is a map with partitions bounds for requested number of partitions
+    private val partition_set_map: HashMap[Int,(Int, Int)] = HashMap.empty[Int,(Int, Int)]
+    private var part_lower_bound: Int = 0
+    private var part_upper_bound: Int = 0
+
+    for ((k, v) <- distribution_map) {
+        part_upper_bound = part_lower_bound + round(v.floatValue * numPartitions) - 1
+        partition_set_map += (k -> (part_lower_bound, part_upper_bound))
+        part_lower_bound = part_upper_bound + 1
+    }
+
+    partition_set_map += (0 -> (part_upper_bound + 1, numPartitions - 1))
+
+
+    override def getPartition(key: Any): Int =
+        {
+            var path_key = key.asInstanceOf[ListBuffer[Int]]
+            val l = path_key.length
+            var partition_set_key: Int = 0
+
+            if (partition_set_map.contains(l)){
+                partition_set_key = l
+            } else {
+                partition_set_key = 0
+            }
+
+            var lower = partition_set_map(partition_set_key)_1
+            var upper = partition_set_map(partition_set_key)_2
+            var out = lower + abs(path_key.hashCode()) % (upper - lower  + 1)
+            out
+        }
+
 }
