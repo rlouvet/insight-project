f15235b4ad665f4d5157c8911aa317146373622f
rlouvet <robinlouvet@gmail.com>
2018-02-23 14:44:56 -0500

    Adding required Flask-WTF package

--- frontend/setup.sh
+++ frontend/setup.sh
@@ -2,4 +2,4 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python-pip python-dev build-essential
-sudo pip install flask tornado boto3
+sudo pip install flask tornado boto3 flask-wtf
